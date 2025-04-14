b34fd2761808798d3fb67f21a054004f7293ab33
Nikon <nikon.ser.par@gmail.com>
2025-04-14 21:30:36 +0300

    start

---
+++ lab2/lib.py
@@ -0,0 +1,27 @@
+import numpy as np
+from autograd import grad
+
+# Градиент функции f в точке
+def gradient(f, point: np.array):
+    grad_f = grad(f)
+    return grad_f(point)
+
+
+class Tracker:
+    def __init__(self) -> None:
+        self.__path = [[], []]
+        self.__iterations = 0
+
+    def track(self, point: np.array) -> None:
+        self.__path[0].append(point[0])
+        self.__path[1].append(point[1])
+
+        self.__iterations += 1
+
+    @property
+    def coordinates(self) -> np.array:
+        return self.__path
+    
+    @property
+    def iterations(self) -> int:
+        return self.__iterations
