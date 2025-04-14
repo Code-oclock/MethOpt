3bc3fdc60a9672800e5c65e4cdb67e0ed0e59b65
Your Name <you@example.com>
2025-04-14 21:55:16 +0300

    add newton

--- lab2/lib.py
+++ lab2/lib.py
@@ -30,3 +30,60 @@ class Tracker:
     @property
     def iterations(self) -> int:
         return self.__iterations
+
+
+def golden_section_search(
+    f, 
+    start: int, 
+    end: int, 
+    tolerance: float, 
+    max_iterations: int
+) -> float:
+    phi = (np.sqrt(5) - 1) / 2  # Золотое сечение ~0.618
+    # Находим две точки c и d, которые делят отрезок [start, end] в пропорции золотого сечения: [start, c, d, end]
+    c = end - (end - start) * phi
+    d = start + (end - start) * phi
+
+    for _ in range(max_iterations):
+        # Если разница между концами отрезка меньше заданной точности, то завершаем поиск
+        if abs(end - start) < tolerance: break
+        # Если значение функции в точке c меньше, чем в точке d, то сдвигаем правую границу (иначе левую)
+        if f(c) < f(d):
+            end = d # сдвигаем правую границу
+            d = c # сдвигаем правую границу разделения отрзка
+            c = end - (end - start) * phi # новая пропорция (левая граница) разделения отрзка
+        else:
+            start = c # сдвигаем левую границу
+            c = d # сдвигаем правую леву. границу разделения отрзка
+            d = start + (end - start) * phi # новая пропорция (правая граница) разделения отрзка
+    middle = (start + end) / 2
+    return middle
+
+def newton_method(
+        f, 
+        point: np.array, 
+        tolerance: float, 
+        max_iterations: int, 
+        tracker: Tracker = None
+) -> np.array:
+    if tracker is not None: tracker.track(point)
+
+    for _ in range(max_iterations):
+        current_gradient = gradient(f, point)
+        if np.linalg.norm(current_gradient) < tolerance:
+            break
+        current_hessian = hessian(f, point)
+
+        # Вычисляем направление: d = - H^(-1) * grad
+        d = np.linalg.solve(current_hessian, current_gradient)
+
+        # Выбор шага методом backtracking line search
+        # g - функция одной переменной (сечение фукнции f плоскостью) - для подбора шага
+        def g(alpha): return f(point - alpha * current_gradient)
+        # Поиск шага методом золотого сечения
+        step = golden_section_search(g, 0, 1, tolerance / 10, max_iterations // 100)
+
+        point -= step * d
+        if tracker is not None: tracker.track(point)
+        
+    return point
